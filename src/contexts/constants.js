140cf077c5fcb3867b4cafce4c4c94f147dd8ace
TungThanhHoang <thanhtung1760@gmail.com>
2022-01-11 08:37:14 +0700

    fix

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl = process.env.NODE_ENV !== "production"
 ? "https://onlineshoppingmarket.herokuapp.com"
-: "deploy"
+: ""
 export const LOCAL_TOKEN_USER = "_user"
 export const LOCAL_TOKEN_NEW_PRODUCTS = "_newproduct"
 export const LOCAL_TOKEN_CART_ITEM = "_cartItem "
